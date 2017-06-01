6c58a63355362169fa99284d609b7ced6e86e6c9
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2017-06-01 16:07:14 -0700

    switch to Schedule Rulesets to be able to edit them

--- buildT24osm.rb
+++ buildT24osm.rb
@@ -36,6 +36,8 @@ def findObject(array,name)
   nil
 end
 
+
+##SCHEDULES
 #read in schedule days and transform to array of hashes
 raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleDays.csv")
 schedule_day_hash = raw_data.map { |row| row.to_hash }
@@ -48,44 +50,80 @@ schedule_day_hash.each do |sch|
   schedule_day.setScheduleTypeLimits(sch_type_limit)
 
   (1..24).each do |i|
-    schedule_day.addValue(OpenStudio::Time.new(0, i, 0, 0), sch[:"#{i}"])
+    if ((i < 24) && (sch[:"#{i}"] == sch[:"#{i+1}"]))
+      #skip to avoid duplicate values
+    else
+      schedule_day.addValue(OpenStudio::Time.new(0, i, 0, 0), sch[:"#{i}"])
+    end    
   end  
 end
 schedule_days = model.getScheduleDays
 
 #read in schedule weeks and transform to array of hashes
-raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleWeeks.csv")
-schedule_week_hash = raw_data.map { |row| row.to_hash }
-#for each schedule week, make a ScheduleWeek object
-schedule_week_hash.each do |sch|
-  schedule_week = OpenStudio::Model::ScheduleWeek.new(model)
-  schedule_week.setName(sch[:scheduleweekname])
-  schedule_week.setMondaySchedule(findObject(schedule_days,sch[:mon]))
-  schedule_week.setTuesdaySchedule(findObject(schedule_days,sch[:tue]))
-  schedule_week.setWednesdaySchedule(findObject(schedule_days,sch[:wed]))
-  schedule_week.setThursdaySchedule(findObject(schedule_days,sch[:thu]))
-  schedule_week.setFridaySchedule(findObject(schedule_days,sch[:fri]))
-  schedule_week.setSaturdaySchedule(findObject(schedule_days,sch[:sat]))
-  schedule_week.setSundaySchedule(findObject(schedule_days,sch[:sun]))
-  schedule_week.setHolidaySchedule(findObject(schedule_days,sch[:hol]))
-  schedule_week.setSummerDesignDaySchedule(findObject(schedule_days,sch[:htgdd]))
-  schedule_week.setWinterDesignDaySchedule(findObject(schedule_days,sch[:clgdd]))
-end
-schedule_weeks = model.getScheduleWeeks
+raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleRulesets_Partial.csv")
+schedule_ruleset_hash = raw_data.map { |row| row.to_hash }
+#for each schedule ruleset, make a ScheduleRuleset object
+schedule_ruleset_hash.each do |sch|
+  schedule_ruleset = OpenStudio::Model::ScheduleRuleset.new(model)
+  schedule_ruleset.setName(sch[:scheduleyearname])
+  schedule_ruleset.defaultDaySchedule.setName("#{sch[:scheduleyearname]} Default")
+  default_day = findObject(schedule_days,sch[:mon])
+  times = default_day.times
+  values = default_day.values
+  i = 0
+  times.each do |t|
+      schedule_ruleset.defaultDaySchedule.addValue(t,values[i])
+    i += 1
+  end
+  
+  #date = OpenStudio::Date.new(OpenStudio::MonthOfYear.new(sch[:endmonth]), sch[:endday])
+  schedule_rule = OpenStudio::Model::ScheduleRule.new(schedule_ruleset, findObject(schedule_days,sch[:sat]))
+  schedule_rule.setName("#{sch[:scheduleyearname]} Sat") 
+  schedule_rule.setApplySaturday(true)
+  schedule_rule = OpenStudio::Model::ScheduleRule.new(schedule_ruleset, findObject(schedule_days,sch[:sun]))
+  schedule_rule.setName("#{sch[:scheduleyearname]} Sun") 
+  schedule_rule.setApplySunday(true)
+  schedule_ruleset.setWinterDesignDaySchedule(findObject(schedule_days,sch[:htgdd]))
+  schedule_ruleset.setSummerDesignDaySchedule(findObject(schedule_days,sch[:clgdd]))
 
-#read in schedule years and transform to array of hashes
-raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleYears_Partial.csv")
-schedule_year_hash = raw_data.map { |row| row.to_hash }
-#for each schedule year, make a ScheduleYear object
-schedule_year_hash.each do |sch|
-  schedule_year = OpenStudio::Model::ScheduleYear.new(model)
-  schedule_year.setName(sch[:scheduleyearname])
+  end
+schedule_rulesets = model.getScheduleRulesets
 
-  #add weeks here.  for now, just first columns
-  date = OpenStudio::Date.new(OpenStudio::MonthOfYear.new(sch[:endmonth]), sch[:endday])
-  schedule_year.addScheduleWeek(date, findObject(schedule_weeks,sch[:schedulewk]))
-end
-schedule_years = model.getScheduleYears
+##Alternative Approach defining Schedule:Week and Schedule:Year objects
+##if using this approach, pass schedule_years instead of schedule_rulesets in the schedule_group loop
+# #read in schedule weeks and transform to array of hashes
+# raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleWeeks.csv")
+# schedule_week_hash = raw_data.map { |row| row.to_hash }
+# #for each schedule week, make a ScheduleWeek object
+# schedule_week_hash.each do |sch|
+  # schedule_week = OpenStudio::Model::ScheduleWeek.new(model)
+  # schedule_week.setName(sch[:scheduleweekname])
+  # schedule_week.setMondaySchedule(findObject(schedule_days,sch[:mon]))
+  # schedule_week.setTuesdaySchedule(findObject(schedule_days,sch[:tue]))
+  # schedule_week.setWednesdaySchedule(findObject(schedule_days,sch[:wed]))
+  # schedule_week.setThursdaySchedule(findObject(schedule_days,sch[:thu]))
+  # schedule_week.setFridaySchedule(findObject(schedule_days,sch[:fri]))
+  # schedule_week.setSaturdaySchedule(findObject(schedule_days,sch[:sat]))
+  # schedule_week.setSundaySchedule(findObject(schedule_days,sch[:sun]))
+  # schedule_week.setHolidaySchedule(findObject(schedule_days,sch[:hol]))
+  # schedule_week.setSummerDesignDaySchedule(findObject(schedule_days,sch[:clgdd]))
+  # schedule_week.setWinterDesignDaySchedule(findObject(schedule_days,sch[:htgdd]))
+# end
+# schedule_weeks = model.getScheduleWeeks
+
+# #read in schedule years and transform to array of hashes
+# raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleYears_Partial.csv")
+# schedule_year_hash = raw_data.map { |row| row.to_hash }
+# #for each schedule year, make a ScheduleYear object
+# schedule_year_hash.each do |sch|
+  # schedule_year = OpenStudio::Model::ScheduleYear.new(model)
+  # schedule_year.setName(sch[:scheduleyearname])
+
+  # #add weeks here.  for now, just first columns
+  # date = OpenStudio::Date.new(OpenStudio::MonthOfYear.new(sch[:endmonth]), sch[:endday])
+  # schedule_year.addScheduleWeek(date, findObject(schedule_weeks,sch[:schedulewk]))
+# end
+# schedule_years = model.getScheduleYears
 
 #read in schedule groups and transform to array of hashes
 raw_data =  CSV.table("#{Dir.pwd}/resources/T24_ScheduleGroups_Partial.csv")
@@ -95,16 +133,18 @@ schedule_group_hash.each do |sch_grp|
   default_schedule_set = OpenStudio::Model::DefaultScheduleSet.new(model)
   default_schedule_set.setName("#{sch_grp[:funcgroup]} Default Schedule Set")
   #add defaults for this group
-  default_schedule_set.setNumberofPeopleSchedule(findObject(schedule_years,sch_grp[:occschref]))
-  default_schedule_set.setElectricEquipmentSchedule(findObject(schedule_years,sch_grp[:recptschref]))
-  default_schedule_set.setHotWaterEquipmentSchedule(findObject(schedule_years,sch_grp[:hotwtrhtgschref]))
-  default_schedule_set.setLightingSchedule(findObject(schedule_years,sch_grp[:intltgregschref]))
-  default_schedule_set.setGasEquipmentSchedule(findObject(schedule_years,sch_grp[:procgasschref]))
-  default_schedule_set.setInfiltrationSchedule(findObject(schedule_years,sch_grp[:infschref]))
-  default_schedule_set.setHoursofOperationSchedule(findObject(schedule_years,sch_grp[:availschref]))
+  default_schedule_set.setNumberofPeopleSchedule(findObject(schedule_rulesets,sch_grp[:occschref]))
+  default_schedule_set.setElectricEquipmentSchedule(findObject(schedule_rulesets,sch_grp[:recptschref]))
+  default_schedule_set.setHotWaterEquipmentSchedule(findObject(schedule_rulesets,sch_grp[:hotwtrhtgschref]))
+  default_schedule_set.setLightingSchedule(findObject(schedule_rulesets,sch_grp[:intltgregschref]))
+  default_schedule_set.setGasEquipmentSchedule(findObject(schedule_rulesets,sch_grp[:procgasschref]))
+  default_schedule_set.setInfiltrationSchedule(findObject(schedule_rulesets,sch_grp[:infschref]))
+  default_schedule_set.setHoursofOperationSchedule(findObject(schedule_rulesets,sch_grp[:availschref]))
 end
 default_schedule_sets = model.getDefaultScheduleSets
 
+
+##SPACE TYPES
 #read in space types and transform to array of hashes
 raw_data =  CSV.table("#{Dir.pwd}/resources/T24_SpaceTypes_Partial.csv")
 space_type_hash = raw_data.map { |row| row.to_hash }
@@ -200,6 +240,8 @@ space_type_hash.each do |spc|
 
 end #end space_type_hash.each 
 
+
+##MATERIALS
 #read in space types and transform to array of hashes
 raw_data =  CSV.table("#{Dir.pwd}/resources/T24_MaterialData.csv")
 material_hash = raw_data.map { |row| row.to_hash }
